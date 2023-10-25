94e4f8feca52729a3834757e3fcf7814d2671e15
Matias Sciancalepore <msciancalepore47@gmail.com>
2023-10-25 17:23:53 -0300

    creo todos los microservicios

---
+++ tpEspecial/microAdmin/src/main/java/com/example/microadmin/MicroAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.microadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroAdminApplication.class, args);
+    }
+
+}
---
+++ tpEspecial/microAdmin/src/test/java/com/example/microadmin/MicroAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

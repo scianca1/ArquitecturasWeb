eb50fb1989d8243881e6936ca6e7b2c4e65042ef
Juan Bertucci <juanbertucci97@gmail.com>
2023-10-26 17:54:18 -0300

    admindto

--- tpEspecial/microAdmin/src/main/java/com/example/microadmin/dtos/AdminDto.java
+++ tpEspecial/microAdmin/src/main/java/com/example/microadmin/dtos/AdminDto.java
@@ -1,5 +1,6 @@
 package com.example.microadmin.dtos;
 
+import com.example.microadmin.entitys.Administrador;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,11 @@ import lombok.Setter;
 public class AdminDto {
     private Long id;
 
+    public AdminDto(Long id) {
+        this.id = id;
+    }
+
+    public AdminDto(Administrador administrador) {
+        this.id= administrador.getId();
+    }
 }
